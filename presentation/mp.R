5c57c9f20ab24b2dfb53a7b813b8008bce8d10bf
Filipe Zabala <35049061+filipezabala@users.noreply.github.com>
2021-08-14 10:43:18 -0300

    Add files via upload

---
+++ presentation/mp.R
@@ -0,0 +1,35 @@
+###
+## Decades of Jurimetrics
+#
+
+inicio <- Sys.time()
+
+# Pacotes via Terminal
+# sudo apt-get install libcurl4-openssl-dev
+
+# Pacotes
+# install.packages(c('tidyverse','e1071','vcd','qcc','forecast','micEcon'), dep = TRUE)
+# update.packages(ask = F)
+
+# Working directory
+getwd()
+setwd('~/Dropbox/PUC/@Extensão/MBA-DS/apresentacao/presentation/')
+options(continue = ' ', digits = 5) # Para retirar os '+' entre as linhas do codigo
+# options(continue = ' ', prompt = ' ') # Para retirar os '+' entre as linhas do codigo e '>' do prompt
+
+# Gerando o arquivo .tex
+Sweave(file = 'mp.Rnw',
+  output = 'mp.tex',
+  encoding = 'utf8')
+
+# dar uma olhada  
+# ?Sweave2knitr
+
+# BibTeX (http://texblog.org/category/sweaveknitr/)
+Sys.setenv(TEXINPUTS = getwd(), BIBINPUTS = getwd(), BSTINPUTS = getwd()) 
+
+# Criando o .pdf a partir do arquivo .tex
+tools::texi2pdf('mp.tex')
+
+fim <- Sys.time()
+fim-inicio
